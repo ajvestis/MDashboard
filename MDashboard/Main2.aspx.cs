5ae0ebac3b64da464f229b4bf0a435834fa5a70b
BLESSED AJ <52662510+ajvestis@users.noreply.github.com>
2021-02-04 13:53:42 -0800

    MDashboard V0.1

---
+++ MDashboard/Main2.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ppmc.Advantage.DataAccess;
+using System.Data;
+
+namespace MDashboard
+{
+    public partial class Main2 : System.Web.UI.Page
+    {
+        private string _conn;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            _conn = Properties.Settings.Default.AJVConnection;
+            DataTable dt = AdVantageData.GetDataTable(_conn, "usp_SummaryReport");
+            lblTotal.Text += Convert.ToInt32(dt.Rows[0]["Total"]).ToString();
+            lblSalesCount.Text += Convert.ToInt32(dt.Rows[0]["SalesCount"]).ToString();
+            lblCreditCard.Text += Convert.ToInt32(dt.Rows[0]["CreditCard"]).ToString();
+            lblCash.Text += Convert.ToInt32(dt.Rows[0]["Cash"]).ToString();
+            lblStoreCredit.Text += Convert.ToInt32(dt.Rows[0]["StoreCredit"]).ToString();
+            lblDiscount.Text += Convert.ToInt32(dt.Rows[0]["Discount"]).ToString();
+            lblReturn.Text += Convert.ToInt32(dt.Rows[0]["Return"]).ToString();
+            
+
+            DataTable dt2 = AdVantageData.GetDataTable(_conn, "usp_HourlyReport");
+
+            dlHourly.DataSource = dt2;
+            dlHourly.DataBind();
+        }
+    }
+}
---
+++ MDashboard/obj/Debug/Package/PackageTmp/Main2.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main2.aspx.cs" Inherits="MDashboard.Main2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>MT-SA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+</head>
+<body>
+    <form id="form1" runat="server">
+   
+    <p>   <asp:Label ID="lblTime" runat="server"></asp:Label></p>
+    <div>
+ 
+    <asp:label ID="lblTotal" runat="server" text="Total: "></asp:label><br />
+    <asp:label ID="lblSalesCount" runat="server" text="S#: "></asp:label><br />
+    <asp:label ID="lblCreditCard" runat="server" text="CC: "></asp:label><br />
+    <asp:label ID="lblCash" runat="server" text="CS: "></asp:label><br />
+    <asp:label ID="lblStoreCredit" runat="server" text="SC: "></asp:label><br />
+    <asp:label ID="lblDiscount" runat="server" text="DC: "></asp:label><br />
+    <asp:label ID="lblReturn" runat="server" text="RT: "></asp:label><br />
+    </div>
+     <h2>Main2</h2>
+    <div>
+        <asp:DataList ID="dlHourly" runat="server">
+        <HeaderTemplate>Hour</HeaderTemplate>
+         <ItemTemplate> <%# Eval("Hour")%> : <%# Eval("HourlyTotal")%>
+       
+    </ItemTemplate>
+        </asp:DataList>
+    </div>
+    </form>
+</body>
+</html>
+
